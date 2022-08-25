41ae4c2a96e5fe23875567ded41f72d94580bf0d
Nicolas Bottin <2019310306@FARROUPILHA.IFRS.EDU.BR>
2022-08-25 09:45:58 -0300

    Adicionando título ao gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -12,8 +12,11 @@ def main():
     
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
+    plt.title('Gráfico de linhas')
 
     plt.plot(valores)
     plt.show()
+
+    
  
 main()
